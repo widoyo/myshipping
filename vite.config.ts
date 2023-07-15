b4d7cf586d39b476408488b002bfebfddbe28fa8
Widoyo <widoyo@gmail.com>
2023-07-15 11:52:37 +0700

    Remove unocss, lucia-auth, better-sqlite, wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
-    UnoCSS(),
     sveltekit(),
   ],
 })
